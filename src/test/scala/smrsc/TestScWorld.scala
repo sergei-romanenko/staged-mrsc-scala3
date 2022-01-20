26975b0f0ad9a70084dcd79b695e26931ae49737
Sergei Romanenko <sergei.romanenko@supercompilers.ru>
2022-01-20 12:25:05 +0300

    TestScWorld: List(c + 1) -> List(List(c + 1))

--- src/test/scala/smrsc/TestScWorld.scala
+++ src/test/scala/smrsc/TestScWorld.scala
@@ -11,11 +11,11 @@ trait TestScWorld extends ScWorld[Int] {
     c1 == c2
 
   override def develop(c: C): List[List[C]] =
-    drive(c) ::: rebuild(c).map(List(_))
+    drive(c) ::: rebuild(c)
 
   def drive(c: C): List[List[C]] =
     if (c < 2) List() else List(List(0, c - 1), List(c - 1))
 
-  def rebuild(c: C): List[C] =
-    List(c + 1)
+  def rebuild(c: C): List[List[C]] =
+    List(List(c + 1))
 }
